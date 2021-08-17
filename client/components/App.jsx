d8a3597c0b1bb93e1090788237e57dc9fb040dc3
jespin457 <jorge.e.espinoza.57@gmail.com>
2021-08-17 17:13:50 -0500

    Renamed Login to SignIn; added more annotations; tidied up some imports

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, createContext, useReducer } from "react";
 import ProductDisplay from "./display_comps/ProductDisplay.jsx";
 import Cart from "./Cart.jsx";
-import Login from "./Login.jsx";
-import Nav from "./NavBar.jsx";
+import SignIn from "./SignIn.jsx";
+import NavBar from "./NavBar.jsx";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SignUp from "./SignUp.jsx";
 
@@ -23,27 +23,30 @@ const App = () => {
 
   return (
     <Router>{/* Enables react-dom-router to switch components*/}
-      <AppContext.Provider value={[state, dispatch]}>
+      <AppContext.Provider value={[state, dispatch]}> {/*THIS IS WHERE REDUX STARTS*/}
         <div className="app-container">
-          <Nav />
+          {/* NAVBAR component */}
+          <NavBar />
           <Switch>
             <Route exact path="/">
-              {/* Product/Display [;ayComponents/Display: holds the MaterialCards components */}
+              {/* Route 1: ProductDisplay Component: holds the MaterialCards components */}
               <ProductDisplay /> 
             </Route>
             <Route exact path="/cart">
-              {/* Display: holds */}
+              {/* Route 2: Cart Component Display: products within cart */}
               <Cart />
             </Route>
             <Route exact path="/signin">
-              <Login />
+              {/* Route 3: Sign InPage */}
+              <SignIn />
             </Route>
+             {/* Route 3.1: This is to sign up/Create User...Subroute is in the componenet */}
             <Route exact path="/signup">
               <SignUp />
             </Route>
           </Switch>
         </div>
-      </AppContext.Provider>
+      </AppContext.Provider> {/* THIS IS WHERE REDUX STARTS*/}
     </Router>//End Router Wrapping- Enables switching of displayed components
   );
 };
