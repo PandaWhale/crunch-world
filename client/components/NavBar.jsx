8d43d5669b9bca9aa6063fa7262207f8c08e0f8d
wmungal <lagnum22@gmail.com>
2021-08-17 16:40:46 -0400

    Modified /App/Navbar/ProductCard

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,24 +1,23 @@
-import React, {useState, useEffect, useReducer, useContext} from 'react';
+import React, { useState, useEffect, useReducer, useContext } from "react";
 import { AppContext } from "./App.jsx";
-import {Link} from 'react-router-dom';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-const Nav = () =>{
-
-    return (
-<nav className="nav-container">
-   <div className="logo">
-<Link to="/"><img src="../../images/logocook.png" id="cooklogo"/></Link>
-
-   </div>
-   <div className="interactables">
-      <Link to="/cart"><ShoppingCartIcon/></Link>
-   <br></br>  <Link to="/signin">Login</Link>
-      
-   </div>
-    
-</nav>
-    )
-} 
-
+import { Link } from "react-router-dom";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+const Nav = () => {
+  return (
+    <nav className="nav-container">
+      <div className="logo">
+        <Link to="/">
+          <img src="../../images/logocook.png" id="cooklogo" />
+        </Link>
+      </div>
+      <div className="interactables">
+        <Link to="/cart">
+          <ShoppingCartIcon />
+        </Link>
+        <br></br> <Link to="/signin"> Wes Login</Link>
+      </div>
+    </nav>
+  );
+};
 
 export default Nav;
