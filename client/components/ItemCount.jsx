bc0ed00f6145333418e879e3dd407856a1288a2d
wmungal <lagnum22@gmail.com>
2021-08-18 14:41:07 -0400

    success thunk request

--- client/components/ItemCount.jsx
+++ client/components/ItemCount.jsx
@@ -1,28 +1,30 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 //we're only importing one action from actions.js
 //If multiple actions, below changes to:
 //import * as actions from '../actions/actions.js'
-import { addItemAction } from '../actions/actions.js';
+import { addItemAction } from "../actions/actions.js";
 
 //this maps properties to props
 const mapStateToProps = (state) => {
   return {
     totalItems: state.mainReducer.totalItems,
-  }
-}
+    productsArray: state.mainReducer.productsArray,
+  };
+};
 
 //this maps 'methods' to props
 //dispatch is an action from Redux
 const mapDispatchToProps = (dispatch) => {
   return {
     addItem: () => {
-      dispatch(addItemAction())
+      dispatch(addItemAction());
     },
-  }
-}
+  };
+};
 
 const ItemCount = (props) => {
+  console.log("HEYYYYYY!", props.productsArray);
   return (
     <span>
       <h5>Items: {props.totalItems}</h5>
@@ -32,8 +34,8 @@ const ItemCount = (props) => {
 };
 
 // export default ItemCount;
-//  export default connect(mapStateToProps, null)(ItemCount); 
-export default connect(mapStateToProps, mapDispatchToProps)(ItemCount); 
+//  export default connect(mapStateToProps, null)(ItemCount);
+export default connect(mapStateToProps, mapDispatchToProps)(ItemCount);
 
 // connect subscribed an individual component (in this case, ItemCount) to the store (single source of truth)
 // connect(mapStateToProps, action)(currentComponent)
--- client/store.js
+++ client/store.js
@@ -1,13 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
-// import thunk from 'thunk';
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
-import reducers from './reducers/index.js';
-// import { loadMarkets } from './actions/actions';
+import reducers from "./reducers/index.js";
+import { renderProductsAction } from "./actions/actions";
 
-const store = createStore(
-  reducers,
-  // applyMiddleware(thunk)
-);
+const store = createStore(reducers, applyMiddleware(thunk));
 
 //consider this as component didmount?
 store.dispatch(renderProductsAction());
