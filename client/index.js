691ccc9286b19123fda2001d23732857fdbf8257
jespin457 <jorge.e.espinoza.57@gmail.com>
2021-08-17 19:36:46 -0500

    Started foundation fr Redux overhaul

--- client/index.js
+++ client/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App.jsx';
+import App from './App.jsx';
 
+// import { Provider } from 'react-redux';
+// import store from './store';
 
 import '../styles/styles.css';
 
