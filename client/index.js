cf12611adaba4d72ef8211934cf77d514f4c18f0
jespin457 <jorge.e.espinoza.57@gmail.com>
2021-08-17 20:52:12 -0500

    Made small progress on Redux; changed client.js to index.js in webpack

--- client/index.js
+++ client/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './App.jsx';
-
-// import { Provider } from 'react-redux';
-// import store from './store';
+import store from './store.js';
 
 import './styles/styles.css';
 
-render( <App />,
-document.getElementById('root'));
+render(
+  <Provider store={store}> 
+    <App />
+  </Provider>,
+document.getElementById('root')
+);
