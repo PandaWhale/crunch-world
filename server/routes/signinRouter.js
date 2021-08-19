72d28f660d71106eaee579f2ee0831b4f6b7e77d
Savitri Beaver <savitribeaver@gmail.com>
2021-08-19 11:05:39 -0500

    added ssid cookie

--- server/routes/signinRouter.js
+++ server/routes/signinRouter.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 //middleware for login requests
 const signinController = require(path.resolve(__dirname, '../controllers/signinController'));
-
+const cookieController = require(path.resolve(__dirname, '../controllers/cookieController'));
 const signinRouter = express.Router();
 
-signinRouter.post('/', signinController.verifyUser, (req, res) => {
+signinRouter.post('/', signinController.verifyUser, cookieController.setSSIDCookie, (req, res) => {
   return res.status(200).set('Content-Type', 'json/application').json(res.locals.signinAttempt)
 });
 
